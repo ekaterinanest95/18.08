3dbf429be4f1142a72ff466b88c8e00a49fa37d2
Ekaterina <katya.kinal@gmail.com>
2022-08-18 21:03:18 +0300

    Ex32

---
+++ Ex32/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 32: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, 
+//и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] array1 = new int[4]; 
+int[] array2 = new int[4]; 
+
+for(int i = 0; i < array1.Length; i++)
+{
+    array1[i] = new Random().Next(-10,20);
+}
+
+Console.WriteLine($"[{String.Join("; ", array1)}]");
+
+
+for(int i = 0; i < array2.Length; i++)
+{
+    array2[i] = -array1[i];
+}
+
+Console.WriteLine($"[{String.Join("; ", array1)}] -> [{String.Join("; ", array2)}]");
+
+
+
+
+
